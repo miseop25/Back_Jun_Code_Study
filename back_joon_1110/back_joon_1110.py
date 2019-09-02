d60a3bf7e437cca4a6f81470d587819748d904e1
minseop <richard25@naver.com>
2019-09-02 22:15:14 +0900

    Update back_joon_1110.py

--- back_joon_1110/back_joon_1110.py
+++ back_joon_1110/back_joon_1110.py
@@ -1,3 +1,16 @@
 N = int(input())
 
-print(N)    
+cnt = 1
+new = N
+
+while True :
+    A = int(new/10)
+    B = int(new%10)
+    C = A + B
+    new = (B*10) + (C%10)
+    if new == N :
+        break
+    else :
+        cnt = cnt + 1
+
+print(cnt)
