aaa462be39597e4d02c855d9c5da33439be5f9cc
minseop <richard25@naver.com>
2019-11-15 01:30:31 +0900

    prepare_Test

---
+++ For_code_Test/prepare_Test/spy_clothes_ver2.py
@@ -0,0 +1,32 @@
+from itertools import combinations
+
+def solution(clothes):
+    answer = 0
+    wear = dict()
+    for i in clothes :
+        #if i[1] in wear.keys() :
+        if wear.get(i[1],None) :        #이렇게 하는 것이 더 효율적이다!
+            wear[i[1]] +=1
+        else :
+            wear[i[1]] = 1
+
+            
+    wear_list = list(wear.values())
+    if len(wear_list) == 30 :
+        return 30
+    if len(wear) != 1 :
+        for i in range(1 ,len(wear_list)+1) :
+            buf = list( combinations(wear_list, i))
+            for k in buf :
+                sum = 1
+                for j in k : 
+                    sum = sum*j
+                answer += sum
+    else :
+        answer =  len(clothes)
+    
+    return answer
+
+
+cl = [['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]
+print(solution(cl))
