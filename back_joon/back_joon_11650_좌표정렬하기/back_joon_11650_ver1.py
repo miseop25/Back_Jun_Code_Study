99e8cd9964de83907711e196ac39c604afd48811
minseop <richard25@naver.com>
2019-11-27 20:48:17 +0900

    Update back_joon_11650_ver1.py

--- back_joon/back_joon_11650_좌표정렬하기/back_joon_11650_ver1.py
+++ back_joon/back_joon_11650_좌표정렬하기/back_joon_11650_ver1.py
@@ -7,7 +7,7 @@ num_listt = []
 for _ in range(N) :
     num_listt.append(tuple(map(int , input().split(' '))))
 
-num_listt = sorted(num_listt, key=itemgetter(0,1))
+num_listt = sorted(num_listt, key=itemgetter(1,0))
 for i in num_listt :
     print(i[0], i[1])
 
