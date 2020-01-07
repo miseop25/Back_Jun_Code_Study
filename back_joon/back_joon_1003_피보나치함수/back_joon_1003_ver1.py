61aa20c32c909ac1ea1f6f95ea8baf4682861289
minseop <richard25@naver.com>
2020-01-07 16:52:49 +0900

    Update back_joon_1003_ver1.py

--- back_joon/back_joon_1003_피보나치함수/back_joon_1003_ver1.py
+++ back_joon/back_joon_1003_피보나치함수/back_joon_1003_ver1.py
@@ -1,6 +1,9 @@
+import sys
+input=sys.stdin.readline
+
 fibonacci = [[1,0], [0,1] ]
 
-for i in range(2 , 40) :
+for i in range(2 , 41) :
     fibonacci.append([])
     fibonacci[i].append(fibonacci[i-1][0] + fibonacci[i-2][0])
     fibonacci[i].append(fibonacci[i-1][1] + fibonacci[i-2][1])
