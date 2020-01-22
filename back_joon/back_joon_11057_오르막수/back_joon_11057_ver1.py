459fc08ce0903168056cae48aa95e0740baa3d97
minseop <richard25@naver.com>
2020-01-23 00:39:36 +0900

    Create back_joon_11057_ver1.py

---
+++ back_joon/back_joon_11057_오르막수/back_joon_11057_ver1.py
@@ -0,0 +1,41 @@
+import collections
+
+def soluction(st_fn,dp_fn) :
+    re_stack = collections.deque()
+
+    buf = dp_fn[1] - dp_fn[0]
+    re_stack.append(buf)
+
+    answer = dp_fn[1] + buf
+
+    for i in st_fn :
+        buf = buf -i
+        re_stack.append(buf)
+        answer += buf
+    
+    return answer, re_stack
+
+    
+
+
+N = int(input())
+dp = collections.deque([10,55])
+stack_list = collections.deque([9,8,7,6,5,4,3,2])
+buf_list = collections.deque()
+
+cnt = 2
+ans = 0
+
+
+if N >2 :
+    while cnt != N :
+        ans, stack_list = soluction(stack_list, dp)
+        dp.popleft()
+        dp.append(ans)
+        cnt +=1
+    print(dp[1]%10007)
+
+
+
+else :
+    print(dp[N-1])
