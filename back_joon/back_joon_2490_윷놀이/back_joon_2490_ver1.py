06cea99a20b10a49587d29bdabfc13565e8b67f1
minseop <richard25@naver.com>
2020-02-02 01:26:19 +0900

    Update back_joon_2490_ver1.py

--- back_joon/back_joon_2490_윷놀이/back_joon_2490_ver1.py
+++ back_joon/back_joon_2490_윷놀이/back_joon_2490_ver1.py
@@ -1,5 +1,5 @@
 for _ in range(3) :
     num_list = list(map(int, input().split(' ')))
-    answer_list = ['E', 'C','B','A','D']
+    answer_list = ['D', 'C','B','A','E']
     ans = sum(num_list)
     print(answer_list[ans])
