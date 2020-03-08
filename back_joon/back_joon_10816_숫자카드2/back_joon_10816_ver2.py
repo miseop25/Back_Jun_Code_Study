830044ee95f8e908de0a0aca309df57f5b7c6716
misseop25 <richard25@naver.com>
2020-03-08 22:06:11 +0900

    Update back_joon_10816_ver2.py

--- back_joon/back_joon_10816_숫자카드2/back_joon_10816_ver2.py
+++ back_joon/back_joon_10816_숫자카드2/back_joon_10816_ver2.py
@@ -33,21 +33,30 @@ def checkEqualRight(card_list, num, bin_index) :
 def checkCard(card_list, num, N) :
     answer = []
     bin_index = N//2
+    pre_bin_index = N
     while True :
         if card_list[bin_index] < num :
-            card_list = card_list[bin_index :]
+            new_bin_index = bin_index + (pre_bin_index-bin_index)//2
+            pre_bin_index = bin_index
         elif card_list[bin_index] > num :
-            card_list = card_list[ : bin_index]
+            if abs(pre_bin_index - bin_index) != 1 :
+                new_bin_index = bin_index - abs((pre_bin_index - bin_index)//2)
+            else :
+                new_bin_index = bin_index - 1
+            pre_bin_index = bin_index
         else :
             answer.append(card_list[bin_index])
             left_answer = len(checkEqualLeft(card_list, num, bin_index))
             right_answer = len(checkEqualRight(card_list, num, bin_index))
             return left_answer + right_answer + 1
 
-        if len(card_list) == 1 :
+        if new_bin_index == bin_index :
             return 0 
+        else :        
+            bin_index = new_bin_index
 
-        bin_index = len(card_list)//2
+
+
 
 
 N = int(input())
