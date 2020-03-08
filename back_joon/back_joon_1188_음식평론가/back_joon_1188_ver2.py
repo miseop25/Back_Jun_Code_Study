20ebd19c627df22f60579fe8c5e1ad0ab3b3ad2a
misseop25 <richard25@naver.com>
2020-03-08 20:01:58 +0900

    Update back_joon_1188_ver2.py

--- back_joon/back_joon_1188_음식평론가/back_joon_1188_ver2.py
+++ back_joon/back_joon_1188_음식평론가/back_joon_1188_ver2.py
@@ -2,15 +2,13 @@ import math
 
 N, M = map(int, input().split(" "))
 
-print(math.gcd(N,M))
-
 
 def knifeCount(sausage, people) :
     if sausage>=people and sausage%people == 0 :
         return 0
     else :
         return people - math.gcd(sausage, people)
-        
+
 
 
 print(knifeCount(N,M))
