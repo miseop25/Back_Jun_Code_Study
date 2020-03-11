ab941c543909eba468949979f268f37ca488bad9
misseop25 <richard25@naver.com>
2020-03-11 19:59:23 +0900

    Update back_joon_11497_ver1.py

--- back_joon/back_joon_11497_통나무건너뛰기/back_joon_11497_ver1.py
+++ back_joon/back_joon_11497_통나무건너뛰기/back_joon_11497_ver1.py
@@ -1,4 +1,6 @@
 import sys
+import collections
+
 input = sys.stdin.readline
 
 T = int(input())
@@ -7,4 +9,18 @@ for _ in range(T) :
     num = int(input())
     tree_list = list(map(int, input().split(" ")))
     tree_list = sorted(tree_list)
-    print(tree_list)
+    deq = collections.deque([])
+
+    for i in range(len(tree_list)-1, -1 , -1) :
+        if i%2 == 1 :
+            deq.appendleft(tree_list[i])
+        else :
+            deq.append(tree_list[i])
+
+    ans = abs(deq[-1] - deq[0])
+    for j in range(1, len(deq)) :
+        buf = abs(deq[j]- deq[j-1])
+        if buf > ans :
+            ans = buf
+    
+    print(ans)
