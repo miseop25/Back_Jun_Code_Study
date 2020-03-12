8d91d10a9283f38e0441fdb390bf0e996e55e9d7
misseop25 <richard25@naver.com>
2020-03-12 19:02:03 +0900

    Update pre_test.py

--- For_code_Test/swmastroTest/pre_test.py
+++ For_code_Test/swmastroTest/pre_test.py
@@ -4,15 +4,36 @@ input = sys.stdin.readline
 N, K = map(int, input().split(" "))
 num_list = list(map(int, input().split(" ")))
 
-one_index = num_list.index(1)
+one_index = num_list.index(1) + 1
+ans_list = []
+
+
+
+
 
 if one_index > K :
-    after_one = (N-one_index-1)//(K-1)
-    before_one = (one_index - 1)//(K-1)
-    ans = after_one + before_one
+    buf_index = one_index-1
+    for i in range(one_index) :
+        after_len = N-one_index-i +1
+        before_len = N -after_len -1
+
+        af_value, af_tmep = divmod(after_len, K-1)
+        if af_tmep != 0 :
+            af_value +=1
+        
+        bef_value, bef_temp = divmod(before_len, K-1)
+        if bef_temp != 0 :
+            bef_value += 1
+
+        ans = af_value + bef_value
+        ans_list.append(ans)
+
+
 else :
-    ans = N//(K-1)
+    value , temp = divmod(N, K-1)
+    if temp != 0 :
+        value += 1 
+    ans_list.append(value)
 
 
-
-print(ans)
+print(min(ans_list))
