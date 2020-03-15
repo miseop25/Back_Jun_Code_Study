baa81b98dccd23b66a4da60809816892c47acd3c
misseop25 <richard25@naver.com>
2020-03-15 21:03:31 +0900

    Update phone_Number_ver1.py

--- Programmers/해시/전화번호 목록/phone_Number_ver1.py
+++ Programmers/해시/전화번호 목록/phone_Number_ver1.py
@@ -2,19 +2,17 @@ def solution(phone_book):
     answer = True
     phone_book = sorted(phone_book, key=len)
     check = 1
-    while  check == 1 :
-        for i in phone_book :
-            temp = len(i)
-            for j in phone_book :
-                if j[:temp] == i :
-                    if i != j :
-                        answer = False
-                        check = -1
-                        break
-            if check == -1 :
-                break
 
-
+    for i in phone_book :
+        temp = len(i)
+        for j in phone_book :
+            if j[:temp] == i :
+                if i != j :
+                    answer = False
+                    check = -1
+                    break
+        if check == -1 :
+            break
     return answer
 
 
