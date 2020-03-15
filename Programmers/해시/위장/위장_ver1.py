599ae5267f0a9aadb047b45cf21c2092ff98dee3
misseop25 <richard25@naver.com>
2020-03-15 19:21:34 +0900

    Create 위장_ver1.py

---
+++ Programmers/해시/위장/위장_ver1.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+
+def solution(clothes):
+    answer = 0
+    wear = dict()
+    for i in clothes :
+        #if i[1] in wear.keys() :
+        if wear.get(i[1],None) :        #이렇게 하는 것이 더 효율적이다!
+            wear[i[1]] +=1
+        else :
+            wear[i[1]] = 1
+
+            
+    wear_list = list(wear.values())
+    if len(wear_list) == 30 :
+        return sum(wear_list)
+    if len(wear) != 1 :
+        for i in range(1 ,len(wear_list)+1) :
+            buf = list( combinations(wear_list, i))
+            for k in buf :
+                sum = 1
+                for j in k : 
+                    sum = sum*j
+                answer += sum
+    else :
+        answer =  len(clothes)
+    
+    return answer
