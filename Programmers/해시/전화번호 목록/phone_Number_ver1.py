7a4fa5aaf0b702983fac3b592e9fe15b1ea468d3
misseop25 <richard25@naver.com>
2020-03-15 19:34:20 +0900

    Create phone_Number_ver1.py

---
+++ Programmers/해시/전화번호 목록/phone_Number_ver1.py
@@ -0,0 +1,21 @@
+def solution(phone_book):
+    answer = True
+    phone_book = sorted(phone_book, key=len)
+    check = 1
+    while  check == 1 :
+        for i in phone_book :
+            temp = len(i)
+            for j in phone_book :
+                if j[:temp] == i :
+                    if i != j :
+                        answer = False
+                        check = -1
+                        break
+            if check == -1 :
+                break
+
+
+    return answer
+
+
+print(solution(['119', '97674223', '1195524421']))
