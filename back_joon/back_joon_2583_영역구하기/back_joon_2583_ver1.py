2711cc02d00e0502fbdf4f9fb2b26691410a5dc5
misseop25 <richard25@naver.com>
2020-03-19 12:40:13 +0900

    Update back_joon_2583_ver1.py

--- back_joon/back_joon_2583_영역구하기/back_joon_2583_ver1.py
+++ back_joon/back_joon_2583_영역구하기/back_joon_2583_ver1.py
@@ -1,18 +1,45 @@
-import sys
-input = sys.stdin.readline
+from collections import deque
 
-M, N, K = map(int, input().split(" "))
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-squre = [[1 for j in range(N)] for i in range(M)]
-wall_list = []
+def bfs(x, y, cnt):
+    q.append([x, y])
+    a[x][y] = cnt
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if a[nx][ny] == 0:
+                    a[nx][ny] = cnt
+                    q.append([nx, ny])
 
-for _ in range(K) :
-    start_x, start_y , end_x, end_y = map(int, input().split(" "))
-    for i in range(start_y, end_y) :
-        for j in range(start_x, end_x) :
-            squre[i][j] = 0
+m, n, k = map(int, input().split())
+a = [[0]*m for _ in range(n)]
+c = a[:]
+q = deque()
+for _ in range(k):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            a[i][j] = -1
 
+cnt = 1
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == 0:
+            bfs(i, j, cnt)
+            cnt += 1
 
-print(squre)
-
-
+cnt -= 1
+print(cnt, end='\n')
+ans = [0] * cnt
+for i in range(n):
+    for j in range(m):
+        if a[i][j] > 0:
+            ans[a[i][j]-1] += 1
+ans.sort()
+for i in range(len(ans)):
+    print("{0} ".format(ans[i]), end='')
