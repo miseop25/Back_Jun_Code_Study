728e5479a515fa77399f5136f3a1b7c15f1e0117
misseop25 <richard25@naver.com>
2020-03-23 17:15:05 +0900

    Create discController_ver1.py

---
+++ Programmers/Heap/디스크 컨트롤러/discController_ver1.py
@@ -0,0 +1,35 @@
+import collections
+
+def solution(jobs):
+    answer = 0
+    jobs = collections.deque(sorted(jobs , key= lambda x: x[1] ))
+    print(jobs)
+    time = 0
+    check = False
+    working = []
+    ans_list = []
+
+    while jobs :
+        if not (working) :
+            for i in range(len(jobs)) :
+                temp, buf = jobs.popleft()
+                if temp <= time :
+                    ans_list.append(time - temp + buf)
+                    working.append(buf)
+                    check = True
+                    break
+                else :
+                    jobs.append([temp, buf])
+            if check :
+                jobs = collections.deque(sorted(jobs , key= lambda x: x[1] ))
+                check = False
+            else :
+                time += 1
+
+        else :
+            time += working.pop()
+    answer = int(sum(ans_list)/len(ans_list))
+        
+    return answer
+
+print(solution([[0, 3], [1, 9], [2, 6]]))
