4251b2ffd2d4dfae796985d3e01814c31791a5da
misseop25 <richard25@naver.com>
2020-03-27 16:32:41 +0900

    Update back_joon_1149_ver2.py

--- back_joon/back_joon_1149_RGB거리/back_joon_1149_ver2.py
+++ back_joon/back_joon_1149_RGB거리/back_joon_1149_ver2.py
@@ -9,21 +9,24 @@ for i in range(3) :
     color_dict[i] = first_house[i]
 
 for _ in range(N-1) :
-    outher_house = list(map(int, input().split(' ')))
+    other_house = list(map(int, input().split(' ')))
     red = color_dict[0]
     green = color_dict[1]
     blue = color_dict[2]
 
     buf = [[],[],[]]
-    buf[0].append(green + outher_house[0])
-    buf[0].append(blue + outher_house[0])
 
-    buf[1].append(red + outher_house[1])
-    buf[1].append(blue + outher_house[1])
+    # red으로 칠할때!
+    buf[0].append(green + other_house[0])
+    buf[0].append(blue + other_house[0])
+    # green으로 칠할때
+    buf[1].append(red + other_house[1])
+    buf[1].append(blue + other_house[1])
+    # blue 로 칠할 때
+    buf[2].append(red + other_house[2])
+    buf[2].append(green + other_house[2])
 
-    buf[2].append(red + outher_house[2])
-    buf[2].append(green + outher_house[2])
-
+    #  최소값 딕셔너리에 다시 저장
     for j in range(3) :
         color_dict[j] = min(buf[j])
 
