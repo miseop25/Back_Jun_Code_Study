7fe9aa54c8011a4c45d637634932145cdcda6a0b
misseop25 <richard25@naver.com>
2020-04-04 19:32:03 +0900

    Create nomal_suare_ver1.py

---
+++ Programmers/기타문제/멀쩡한 사각형/nomal_suare_ver1.py
@@ -0,0 +1,18 @@
+def solution(w,h):
+    answer = 0
+    if w == h :
+        return w*h - w
+    elif w == 1 or h == 1 :
+        return 0
+    
+    if w < h :
+        x = w 
+        y = h
+    else :
+        x = h
+        y = w 
+    val , leave = divmod(y, x)
+    if leave != 0 :
+        val += 1
+    answer = (w*h) - (val * x)
+    return answer
