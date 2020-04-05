5e7b6da1431af401f777d6684d0c56def35d15e7
misseop25 <richard25@naver.com>
2020-04-05 23:39:35 +0900

    Create back_joon_3448_ver1.py

---
+++ back_joon/back_joon_3448_문자인식/back_joon_3448_ver1.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(N):
+    word = ""
+    check_error = 0
+    enter = 0
+    while True :
+        buf = input()
+        if buf != "\n" :
+            word += buf
+            enter += 1
+        else :
+            break
+    for j in word :
+        if j == "#" :
+            check_error += 1
+    length = len(word) - enter
+    rate = round(((length - check_error)/length)*100, 1)
+    rate = str(rate)
+    if rate[-1] == "0" :
+        rate = rate[: -2]
+    rate += "%."
+    print("Efficiency ratio is", rate)
+
+    
+
