3f99591c442b5922de21cb5ec2fdf4703d0e905e
misseop25 <richard25@naver.com>
2020-04-10 20:44:06 +0900

    Update back_joon_2579_ver1.py

--- back_joon/back_joon_2579_계단오르기/back_joon_2579_ver1.py
+++ back_joon/back_joon_2579_계단오르기/back_joon_2579_ver1.py
@@ -17,6 +17,7 @@ def soluction(N) :
         case1 = []
         case2 = []
         case3 = []
+        buf = []
         for i in answer :
             if i[1] == 1 :
                 case3.append([i[0]+ temp, 2])
@@ -30,11 +31,12 @@ def soluction(N) :
         case2 = sorted(case2, key= lambda x: x[0], reverse=True)
         case3 = sorted(case3, key= lambda x: x[0], reverse=True)
         if case1 :
-            answer.append(case1[0])
+            buf.append(case1[0])
         if case2 :
-            answer.append(case2[0])
+            buf.append(case2[0])
         if case3 : 
-            answer.append(case3[0])
+            buf.append(case3[0])
+        answer = buf
 
     last = int(input())
     for i in answer :
