49402bc86a6c4e4e44549d721aa34b92d2261ee7
misseop25 <richard25@naver.com>
2020-04-18 16:30:20 +0900

    Update back_joon_5525_ver2.py

--- back_joon/back_joon_5525_IOIOI/back_joon_5525_ver2.py
+++ back_joon/back_joon_5525_IOIOI/back_joon_5525_ver2.py
@@ -6,18 +6,21 @@ M = int(input())
 string = input().rstrip()
 answer = 0
 i = -1
-while i < M-3 :
-    i += 1
-    if string[i] == "I" :
-        o_cnt = 0
-        while string[i+1] == "O" and string[i+2] == "I" :
-            o_cnt += 1
-            i += 2
-            if o_cnt == N :
-                o_cnt -= 1
-                answer += 1
-            if i > M-2 :
-                break
+try :
+    while i < M-2 :
+        i += 1
+        if string[i] == "I" :
+            o_cnt = 0
+            while string[i+1] == "O" and string[i+2] == "I" :
+                o_cnt += 1
+                i += 2
+                if o_cnt == N :
+                    o_cnt -= 1
+                    answer += 1
+                if i > M-2 :
+                    break
+except :
+    pass
 
 print(answer)
 
