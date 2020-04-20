aadc6dd7e9285ca407943f5b6c3173154919f75c
misseop25 <richard25@naver.com>
2020-04-20 19:40:57 +0900

    Create back_joon_10828_ver1.py

---
+++ back_joon/스택/back_joon_10828_스택/back_joon_10828_ver1.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+
+def soluction(command) :
+    if command[0] == "push" :
+        stack.append(int(command[1]))
+    elif command[0] == "pop" :
+        if stack :
+            print(stack.pop())
+        else :
+            print(-1)
+    elif command[0] == "size" :
+        print(len(stack))
+    elif command[0] == "empty" :
+        if stack :
+            print(0)
+        else :
+            print(1)
+    elif command[0] == "top" :
+        if stack :
+            print(stack[-1])
+        else :
+            print(-1)
+
+        
+
+N = int(input())
+stack = []
+
+for _ in range(N) :
+    cmd = list(map(str, input().rstrip().split(" ")))
+    soluction(cmd)
+
