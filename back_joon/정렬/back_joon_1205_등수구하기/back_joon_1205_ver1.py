3276dd1b7bac68785f4f86a5687482b58f1018c9
misseop25 <richard25@naver.com>
2020-04-28 12:54:09 +0900

    Create back_joon_1205_ver1.py

---
+++ back_joon/정렬/back_joon_1205_등수구하기/back_joon_1205_ver1.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N, s, p = map(int, input().split(" "))
+if N == 0 :
+    score_list = []
+else :
+    score_list = list(map(int, input().split(" ")))
+answer = [0, 0]
+if s in score_list :
+    temp = score_list.index(s)
+    answer[1] = temp + 1
+    cnt = 0
+    for i in score_list[temp : ] :
+        if i == s :
+            cnt += 1 
+        else : 
+            break
+    answer[0] = temp + cnt + 1
+else :
+    score_list.append(s)
+    score_list = sorted(score_list, reverse= True)
+    answer[0] = score_list.index(s) + 1
+    answer[1] = score_list.index(s) + 1
+
+if answer[0] <= p :
+    print(answer[1])
+else :
+    print(-1)
+
+        
