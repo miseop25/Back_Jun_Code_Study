56b9037b29460b6e861a723242d868468013a997
misseop25 <richard25@naver.com>
2020-05-03 13:32:55 +0900

    Update dart_game_ver1.py

--- Programmers/기타문제/다트게임/dart_game_ver1.py
+++ Programmers/기타문제/다트게임/dart_game_ver1.py
@@ -3,6 +3,8 @@ def solution(dartResult):
     score_list = []
     tmep = 0
     stack = []
+
+    #  문자열을 3개로 나누는 작업 
     for i in range(1, len(dartResult) - 1 ) :
         if dartResult[i] >= "0" and dartResult[i] <= "9" :
             if stack :
@@ -11,11 +13,9 @@ def solution(dartResult):
                 stack = []
         else :
             stack.append(dartResult[i])
-        
-
     score_list.append(dartResult[tmep : ])
-    print(score_list)
 
+    # 나눈 문자열을 조건에 따라 점수를 부여하는 작업
     for s in range(3) :
         num_buf = ''
         bonus = 0
@@ -33,9 +33,10 @@ def solution(dartResult):
                 option.append(-1)
             elif i == "*" :
                 option.append(2)
-
+        #  점수와 bonus는 항상 존재하지만 option은 없을 수도 있기 때문에 배열에 저장
         score = pow(int(num_buf), bonus)
         if option :
+            #  option이 # 인 경우는 -1을 곱하고 *인 경우는 2를 본 점수와 이전 점수에 곱한다.
             if option[0] == -1 :
                 score = score*-1
             else :
