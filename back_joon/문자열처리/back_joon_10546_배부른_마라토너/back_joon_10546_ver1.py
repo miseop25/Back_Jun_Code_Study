4b30492295b5cd4b87633fbc9ef9df79eae3d5ba
misseop25 <richard25@naver.com>
2020-05-14 17:35:53 +0900

    Update back_joon_10546_ver1.py

--- back_joon/문자열처리/back_joon_10546_배부른_마라토너/back_joon_10546_ver1.py
+++ back_joon/문자열처리/back_joon_10546_배부른_마라토너/back_joon_10546_ver1.py
@@ -2,6 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+# person : 참가한 사람들의 dict
+# finish : 완주한 사람들의 dict
 person = dict()
 finish = dict()
 answer = ""
@@ -19,11 +21,14 @@ for _ in range(N-1) :
     else :
         finish[name] = 1
 
+
 for n in person.keys() :
+    #  참가자에는 있지만 완주자에는 없다면 해당 키가 정답
     if n not in finish :
         answer = n
         break
     else :
+        #  참가자의 value와 완주자의 value가 다르다면 해당 키가 정답
         if person[n] != finish[n] :
             answer = n
             break
