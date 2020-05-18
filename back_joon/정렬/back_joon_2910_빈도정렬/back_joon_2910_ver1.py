abdc8b35568fddaf3c73bae6d9bcb0366304f645
misseop25 <richard25@naver.com>
2020-05-18 19:49:10 +0900

    back_joon_2910 python

---
+++ back_joon/정렬/back_joon_2910_빈도정렬/back_joon_2910_ver1.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N, C = map(int, input().split(" "))
+    num = list(map(int, input().split(" ")) )
+    num_dict = dict()
+    for i in range(N) :
+        if num[i] in num_dict :
+            num_dict[num[i]][0] += 1
+        else :
+            num_dict[num[i]] = [1, i]
+    
+    result = sorted(num_dict.items(), key= lambda x: (-x[1][0], x[1][1] ))
+    answer = []
+    print(result)
+    for i in result :
+        for j in range(i[1][0]) :
+            answer.append(str(i[0]))
+    print(" ".join(answer))
+        
+
