13bbf13e2cb3c7d317e6571ce3ed1f64e1d15181
misseop25 <richard25@naver.com>
2020-06-02 14:19:35 +0900

    Create back_joon_2872_ver2.py

---
+++ back_joon/구현/back_joon_2872_우리집엔 도서관이 있어/back_joon_2872_ver2.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+def soluction(num) :
+    books = []
+    result = 0
+    for i in range(num) :
+        books.append( int(input()))
+    
+    cnt  = 1
+    check = False
+    for i in range(len(books)-1, -1, -1 ) :
+        tenp = books[i]
+        if tenp == N :
+            check = True
+        if check :
+            if books[i] == num -1 :
+                cnt += 1
+                num -= 1
+    result = len(books) - cnt
+    return result
+
+
+
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+    print(soluction(N))
