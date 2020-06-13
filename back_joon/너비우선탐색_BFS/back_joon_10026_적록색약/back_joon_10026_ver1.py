ecc5651d33b61c2dc1ed73d513b50f010d99e891
misseop25 <richard25@naver.com>
2020-06-13 21:23:52 +0900

    Update back_joon_10026_ver1.py

--- back_joon/너비우선탐색_BFS/back_joon_10026_적록색약/back_joon_10026_ver1.py
+++ back_joon/너비우선탐색_BFS/back_joon_10026_적록색약/back_joon_10026_ver1.py
@@ -4,9 +4,8 @@ input = sys.stdin.readline
 def checkImage(x, y,color) :
     dx = [-1, 1, 0,0]
     dy = [0, 0, -1, 1]
-    cnt = 0
 
-    BFS[x][y] = 1
+    BFS[x][y] = 1 
     for i in range(4): 
         tx = x + dx[i]
         ty = y + dy[i]
@@ -14,39 +13,65 @@ def checkImage(x, y,color) :
             if board[tx][ty] == color and BFS[tx][ty] == 0 :
                 BFS[tx][ty] = 1
                 checkImage(tx, ty, color)
-                cnt = 1
-    return cnt
+    return 1
+
+def checkRGImage(x, y, color) :
+    dx = [-1, 1, 0,0]
+    dy = [0, 0, -1, 1]
+
+    RG_BFS[x][y] = 1
+    for i in range(4): 
+        tx = x + dx[i]
+        ty = y + dy[i]
+        if tx >= 0 and tx < N and ty >= 0 and ty < N :
+            if color == "RG" :
+                if (board[tx][ty] == "R" or board[tx][ty] == "G") and RG_BFS[tx][ty] == 0 :
+                    RG_BFS[tx][ty] = 1
+                    checkRGImage(tx, ty, color)
+            else :
+                if board[tx][ty] == color and RG_BFS[tx][ty] == 0 :
+                    RG_BFS[tx][ty] = 1
+                    checkRGImage(tx, ty, color)
+    return 1
     
+
 
 
 if __name__ == "__main__":
     N = int(input())
-    rgPerson = {"rg" : [], "b" : []}
+    rgPerson = {"RG" : [], "B" : []}
     normalPerson = {"R": [], "G" : [], "B" : []}
     BFS = [[0 for i in range(N)] for j in range(N)]
+    RG_BFS = [[0 for i in range(N)] for j in range(N)]
     board = []
 
     for i in range(N) :
         temp = input().rstrip()
         for j in range(N) :
             if temp[j] == "R" :
-                rgPerson["rg"].append([i,j])
+                rgPerson["RG"].append([i,j])
                 normalPerson["R"].append([i,j])
             elif temp[j] == "G" :
-                rgPerson["rg"].append([i,j])
+                rgPerson["RG"].append([i,j])
                 normalPerson["G"].append([i,j])
             else :
-                rgPerson["b"].append([i,j])
+                rgPerson["B"].append([i,j])
                 normalPerson["B"].append([i,j])
         board.append(temp)
     
-    print(board)
-
     norAnswer = 0
     rgAnswer = 0
 
     for n in normalPerson.keys() :
         for x, y in normalPerson[n] :
-            norAnswer += checkImage(x,y, n)
-    print(norAnswer)
+            if BFS[x][y] != 1 :
+                norAnswer += checkImage(x,y, n)
+    
+    for rg in rgPerson.keys() :
+        for x, y in rgPerson[rg] :
+            if RG_BFS[x][y] != 1 :
+                rgAnswer += checkRGImage(x, y, rg)
+    
+
+    print(norAnswer, rgAnswer)
             
