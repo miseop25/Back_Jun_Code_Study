ff3e8a7640185033629e62fca18298eeea897c3d
misseop25 <richard25@naver.com>
2020-06-19 23:44:33 +0900

    Create changeBracket_ver1.py

---
+++ Programmers/기타문제/괄호변환/changeBracket_ver1.py
@@ -0,0 +1,26 @@
+def solution(p):
+    answer = []
+    left = []
+    for i, var in enumerate(p) :
+        if var == "(" :
+            answer.append(var)
+            left.append(i)
+        else :
+            if left :
+                answer.append(")")
+                left.pop()
+            else :
+                left.append(i)
+                answer.append("(")
+    if left :
+        temp = len(left)//2
+        for i in left[: temp] :
+            answer[i + temp] = ")"
+            temp -= 1
+
+
+
+
+    return "".join(answer)
+
+print(solution("()))((()"))
