bd504dfdc0337ac7ba914f9bda5f060766006efb
misseop25 <richard25@naver.com>
2020-06-21 16:08:45 +0900

    Create rightBracket_ver1.py

---
+++ Programmers/Stack_Que/올바른괄호/rightBracket_ver1.py
@@ -0,0 +1,14 @@
+def solution(s):    
+    stack = []
+    for i in s :
+        if i == "(" :
+            stack.append(i)
+        else :
+            if stack :
+                stack.pop()
+            else :
+                return False
+    if stack :
+        return False
+    else :
+        return True
