3e9b47e6a689509c3a9dd4722bb968b1a2b674d6
misseop25 <richard25@naver.com>
2020-07-01 21:14:14 +0900

    프로그래머스 소수만들기 - Python

---
+++ Programmers/기타문제/소수만들기/makePrimeNumber_ver1.py
@@ -0,0 +1,19 @@
+import itertools
+def solution(nums):
+    answer = 0
+    result = list(itertools.combinations(nums, 3))
+    prims = [True for i in range(3001)]
+    prims[0] = False
+    prims[1] = False
+    for i in range(2 , 3001) :
+        if prims[i] :
+            for j in range(2*i, 3001, i) :
+                prims[j] = False
+    
+    for i in result :
+        temp = sum(i)
+        if prims[temp] :
+            answer += 1
+    
+
+    return answer
