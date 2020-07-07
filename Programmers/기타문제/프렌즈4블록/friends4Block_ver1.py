a89450a92536c9b36a7e50ea8af65f8b4ed1e6d7
misseop25 <richard25@naver.com>
2020-07-07 23:34:35 +0900

    Update friends4Block_ver1.py

--- Programmers/기타문제/프렌즈4블록/friends4Block_ver1.py
+++ Programmers/기타문제/프렌즈4블록/friends4Block_ver1.py
@@ -5,6 +5,7 @@ def find4Block(m, n ,board) :
     for i in range(1, m) :
         for j in range(1, n) :
             flag = True
+            if board[i][j] == "X" : continue
             comp = board[i][j]
             for k in range(3) :
                 x = i + dx[k]
@@ -18,20 +19,34 @@ def find4Block(m, n ,board) :
                     x = i + dx[k]
                     y = j + dy[k]
                     delBlock.add((x, y))
+    if len(delBlock) == 0 :
+        return False
     return delBlock
 
 def blockDown(m,n, board) :
     for i in range(m-1) :
         for j in range(n) :
-            if board[i+1][j] == "X" and board[i][j] != "X":
-                temp = board[i][j]
+            if board[i+1][j] == "X" and board[i][j] != "X" :
+                temp = []
+                for t in range(i, -1, -1) :
+                    if board[t][j] != "X" :
+                        temp.append(board[t][j])
+                        board[t][j] = "X"
+                    else :
+                        break
                 board[i][j] = "X"
                 for k in range(i+1, m) :
                     if board[k][j] != "X" :
-                        board[k-1][j] = temp
+                        for t in range(k - len(temp), k ) :
+                            board[t][j] = temp.pop()
+
                         break
                     else :
                         board[k][j] = "X"
+                if temp :
+                    for t in range(m- len(temp), m) :
+                        board[t][j] = temp.pop()
+                
     return board
     
 def delectBlock(d, board) :
@@ -50,11 +65,15 @@ def solution(m, n, board):
         game.append(list(i))
 
     delList = find4Block(m,n,game)
+    if delList != False :
+        answer += len(delList)
     while delList :
         game = delectBlock(delList, game)
         game = blockDown(m,n, game)
         delList = find4Block(m,n,game)
+        if delList != False :
+            answer += len(delList)
     return answer
 
 
-print(solution(6,6,["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"] ))
+print(solution(4,4,["ABCD", "BACE", "BCDD", "BCDD"] ))
