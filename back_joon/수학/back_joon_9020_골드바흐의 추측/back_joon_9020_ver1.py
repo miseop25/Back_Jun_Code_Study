a689b5f9711b841990d1f26f01e82e5083c951a3
misseop25 <richard25@naver.com>
2020-07-20 20:29:33 +0900

    Update back_joon_9020_ver1.py

--- back_joon/수학/back_joon_9020_골드바흐의 추측/back_joon_9020_ver1.py
+++ back_joon/수학/back_joon_9020_골드바흐의 추측/back_joon_9020_ver1.py
@@ -1,21 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-prime = [True for i in range(10001)]
 def getPrimeNumber() :
     answer = dict()
+    prime = [True for i in range(10001)]
     for i in range(2, 10001) :
         if prime[i] :
             answer[i] = True
             for j in range(i*2, 10001, i) :
                 prime[j] = False
     return answer
+
 num = getPrimeNumber()
 
 def soluction() :
     n = int(input())
     result = []
-    for i in range(n-1, 1, -1) :
+    for i in range(n-1, n//2 - 1, -1) :
         if i in num and (n-i) in num :
             result.append([i, n-i])
 
