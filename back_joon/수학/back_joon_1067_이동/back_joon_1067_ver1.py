cd85bbb6663d78609f34066ff8ce00e781f234ba
misseop25 <richard25@naver.com>
2020-07-21 10:32:36 +0900

    Create back_joon_1067_ver1.py

---
+++ back_joon/수학/back_joon_1067_이동/back_joon_1067_ver1.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    x = deque(list(map(int, input().split(" "))))
+    y = deque(list(map(int, input().split(" "))))
+    m1 = min(x)
+    m2 = min(y)
+    while x[0] != m1 or y[0] != m2 :
+        if x[0] != m1 :
+            buf = x.popleft()
+            x.append(buf)
+        if y[0] != m2 :
+            buf = y.popleft()
+            y.append(buf)
+    answer = 0
+    for i in range(N) :
+        answer += x[i]*y[i]
+    print(answer)
+            
