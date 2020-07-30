d714f96de6c9f67543ea7e58b0476e8c99c85861
misseop25 <richard25@naver.com>
2020-07-30 14:36:33 +0900

    Create back_joon_1614_ver1.py

---
+++ back_joon/수학/back_joon_1614_영식이의_손가락/back_joon_1614_ver1.py
@@ -0,0 +1,36 @@
+def soluction(n, cnt) :
+    finger = [[8], [6,2], [4], [2,6], [8]]
+    answer = 0
+    if cnt == 1 :
+        answer += n + finger[n-1][0] - 1
+        return answer
+    
+    if n == 1 or n == 3 or n == 5 :
+        answer += n + (cnt)*sum(finger[n-1]) - 1
+        return answer
+    elif n == 2 :
+        v, r= divmod(cnt+1, 2)
+        answer = v*8 - 1
+        if r != 0 :
+            if cnt%2 == 0 :
+                answer += finger[n-1][1]
+            else :
+                answer += finger[n-1][0]    
+    else :
+        v, r= divmod(cnt, 2)
+        answer += n + v*8 - 1
+        if r != 0 :
+            if cnt%2 == 0 :
+                answer += finger[n-1][1]
+            else :
+                answer += finger[n-1][0]   
+
+
+
+    return answer
+
+
+if __name__ == "__main__":
+    n = int(input())
+    cnt = int(input())
+    print(soluction(n,cnt))
