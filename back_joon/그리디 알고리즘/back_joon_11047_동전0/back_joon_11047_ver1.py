e47274d666bc0fcc752f9db41479cd3c7c66de7d
misseop25 <richard25@naver.com>
2020-07-31 12:21:44 +0900

    Create back_joon_11047_ver1.py

---
+++ back_joon/그리디 알고리즘/back_joon_11047_동전0/back_joon_11047_ver1.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def soluction(K, coin) :
+    for i in coin :
+        if K - i >= 0 :
+            v, r = divmod(K, i)
+            return v, r
+
+if __name__ == "__main__":
+    N, K = map(int, input().split(" "))
+    coin = []
+    for _ in range(N) :
+        coin.append(int(input()))
+    coin = sorted(coin, key= lambda x: -x)
+    answer = 0
+    while K > 0 :
+        a, K = soluction(K, coin)
+        answer += a
+    print(answer)
+    
+
+    
