224865909bd5f9b68d8020d159ae160e74378c6b
misseop25 <richard25@naver.com>
2020-08-03 12:03:33 +0900

    Create arrangingCoin_ver1.py

---
+++ LeetCode/ArrangingCoin/arrangingCoin_ver1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self) :
+        self.dp = [1]
+
+    def updateDP(self, num: int) -> None :
+        index = len(self.dp) + 1
+        while self.dp[-1] <= num :
+            self.dp.append(self.dp[-1]+ index)
+            index += 1
+
+
+    def arrangeCoins(self, n: int) -> int:
+        if self.dp[-1] < n :
+            self.updateDP(n)
+            return len(self.dp)
+        for i , val in enumerate(self.dp):
+            if val > n :
+                return i
+
+
+
+t = Solution()
+print(t.arrangeCoins(5))
+print(t.arrangeCoins(1))
