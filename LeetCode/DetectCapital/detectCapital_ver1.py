2c2d1377d35cfc2f7f2a70c50a4e6dbd6821c8d6
misseop25 <richard25@naver.com>
2020-08-03 12:03:36 +0900

    Update detectCapital_ver1.py

--- LeetCode/DetectCapital/detectCapital_ver1.py
+++ LeetCode/DetectCapital/detectCapital_ver1.py
@@ -3,8 +3,7 @@ class Solution:
 
         c1 = word.upper()
         c2 = word.lower()
-        c3 = c1[0] + c2[1 :]
-
+        c3 = word.capitalize()
         if c1 == word or c2 == word or c3 == word :
             return True
         else :
