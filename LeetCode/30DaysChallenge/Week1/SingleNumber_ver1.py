c7f8ea71052faf6db5f4c3a77a10d86d7f10e1ab
misseop25 <richard25@naver.com>
2020-08-06 20:21:09 +0900

    Update SingleNumber_ver1.py

---
+++ LeetCode/30DaysChallenge/Week1/SingleNumber_ver1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+
+        if len(nums) == 1 :
+            return nums[0]
+        if nums[0] != nums[1] :
+            return nums[0]
+
+        for i in range(1, len(nums)-1) :
+            if nums[i] != nums[i-1] and nums[i] != nums[i+1] :
+                return nums[i]
+        return nums[-1]
