cc1c2aef159dd4d15100d0b521b60c489e7b6477
misseop25 <richard25@naver.com>
2020-08-06 20:21:22 +0900

    Create find_all_duplicates_ver1.py

---
+++ LeetCode/Find_All_Duplicates_In_Array/find_all_duplicates_ver1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        if not nums :
+            return nums
+        nums.sort()
+        answer = []
+        cnt = 1
+        comp = nums[0]
+        for i in nums[1:] :
+            if comp == i :
+                cnt += 1
+            else :
+                if cnt == 2 :
+                    answer.append(comp)
+                cnt = 1
+                comp = i
+        if cnt == 2 :
+            answer.append(comp)
+        return answer
+
