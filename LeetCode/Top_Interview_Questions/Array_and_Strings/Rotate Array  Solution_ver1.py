eede8057db335e0b85fd02b3ab441e6ad9d34911
misseop25 <richard25@naver.com>
2020-08-07 18:15:46 +0900

    Update Rotate Array  Solution_ver1.py

--- LeetCode/Top_Interview_Questions/Array_and_Strings/Rotate Array  Solution_ver1.py
+++ LeetCode/Top_Interview_Questions/Array_and_Strings/Rotate Array  Solution_ver1.py
@@ -2,7 +2,6 @@ from typing import List
 from collections import deque
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        nums = deque(nums)
         for _ in range(k) :
             temp = nums.pop()
-            nums.appendleft(temp)
+            nums.insert(0, temp)
