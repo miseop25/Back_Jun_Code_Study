977a18b564a309a44a42b07e18fd9fa31a7b1d26
misseop25 <richard25@naver.com>
2020-08-18 23:26:44 +0900

    Create back_joon_4195_ver1.py

---
+++ back_joon/트리/back_joon_4195_친구네트워크/back_joon_4195_ver1.py
@@ -0,0 +1,16 @@
+import sys
+import copy
+input = sys.stdin.readline
+
+
+def soluction() :
+    nameDict = dict()
+    friendDict = dict()
+
+    case = int(input())
+    for _ in range(case) :
+
+
+if __name__ == "__main__":
+    N = int(input())
+
