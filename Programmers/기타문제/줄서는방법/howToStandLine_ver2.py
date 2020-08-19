d828926fa6b32ffb3f6273cc9fa6c613db2a255d
misseop25 <richard25@naver.com>
2020-08-19 20:24:56 +0900

    Update howToStandLine_ver2.py

--- Programmers/기타문제/줄서는방법/howToStandLine_ver2.py
+++ Programmers/기타문제/줄서는방법/howToStandLine_ver2.py
@@ -3,8 +3,7 @@ def solution(n, k):
     answer = []
     num = [i for i in range(1, n+1)]
     while num :
-        total = math.factorial(len(num))
-        mid = total/len(num)
+        mid = math.factorial(len(num) - 1)
         v, r = divmod(k,mid)
         if r == 0 : v -= 1
         answer.append(num.pop(int(v)))
