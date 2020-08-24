f8cf8a04db8e81fa71ad801ca07354320b03cc4b
misseop25 <richard25@naver.com>
2020-08-24 21:18:49 +0900

    Create back_joon_10808_ver1.py

---
+++ back_joon/문자열처리/back_joon_10808_알파벳개수/back_joon_10808_ver1.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    word = input().rstrip()
+    alpa = ['0']*26
+    for i in word :
+        alpa[ord(i)- 97] = str(int(alpa[ord(i)- 97]) + 1)
+    answer = " ".join(alpa)
+    print(answer)
