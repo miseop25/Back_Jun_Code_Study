54fb784c697175fe78391b387e9d9ccbc6a6c5f7
misseop25 <richard25@naver.com>
2020-09-08 21:34:33 +0900

    Create multMatrix_ver2.py

---
+++ Programmers/기타문제/행렬의곱셈/multMatrix_ver2.py
@@ -0,0 +1,18 @@
+def solution(arr1, arr2):
+    answer = []
+    y = len(arr1)
+    x = len(arr2[0])
+
+
+    for i in range(y) :
+        answer.append([])
+        for j in range(x) :
+            value = 0
+            for k in range(x) :
+                value += arr1[i][k]*arr2[k][j]
+            answer[i].append(value)
+
+    return answer
+
+
+print(solution([[1, 4], [3, 2], [4, 1]], [[3, 3], [3, 3]]))
