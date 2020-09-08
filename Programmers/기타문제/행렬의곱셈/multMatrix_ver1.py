267e6996323c320d656b840e45d3af2fffe1ce49
misseop25 <richard25@naver.com>
2020-09-08 21:34:30 +0900

    Update multMatrix_ver1.py

--- Programmers/기타문제/행렬의곱셈/multMatrix_ver1.py
+++ Programmers/기타문제/행렬의곱셈/multMatrix_ver1.py
@@ -4,14 +4,6 @@ def solution(arr1, arr2):
     result = numpy.matmul(arr1, arr2)
     for i in result :
         answer.append(list(i))
-    # for value in arr1 :
-    #     temp = []
-    #     for i in range(len(value)) :
-    #         buf = 0
-    #         for j in range(len(value)) :
-    #             buf += value[j]*arr2[j][i]
-    #         temp.append(buf)
-    #     answer.append(temp)
 
     return answer
 
