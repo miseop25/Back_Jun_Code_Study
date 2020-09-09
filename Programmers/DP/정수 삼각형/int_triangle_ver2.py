57bcddf2db6692de657357bb25b2424d881a5d4a
misseop25 <richard25@naver.com>
2020-09-09 13:22:53 +0900

    Create int_triangle_ver2.py

---
+++ Programmers/DP/정수 삼각형/int_triangle_ver2.py
@@ -0,0 +1,25 @@
+def sum_cal(prelist, afterlist) :
+    ans_list = []
+    num = len(prelist)
+    for i in range(num) :
+        if i == 0 :
+            ans_list.append(prelist[i] + afterlist[i])
+        elif i > 0  and i < num-1 :
+            first = prelist[i-1] + afterlist[i]
+            second = prelist[i] + afterlist[i]
+            ans_list.append(max(first, second))
+        elif i == num-1 :
+            ans_list.append(prelist[i] + afterlist[i+1])
+    return ans_list
+
+
+def solution(triangle):
+    answer = 0
+    if len(triangle) == 1 :
+        return triangle[0][0]
+    else :
+        ans_list = triangle[0]
+        for i in range(1, len(triangle)) :
+            ans_list = sum_cal(ans_list, triangle[i])
+    answer = max(ans_list)
+    return answer
