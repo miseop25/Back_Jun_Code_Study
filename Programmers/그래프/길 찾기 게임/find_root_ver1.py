242aa8c0a68c0d7f4bb24aee1c1cb8453b529f23
misseop25 <richard25@naver.com>
2020-09-09 17:03:01 +0900

    Create find_root_ver1.py

---
+++ Programmers/그래프/길 찾기 게임/find_root_ver1.py
@@ -0,0 +1,46 @@
+from collections import deque
+class Node :
+    def __init__(self,index, x) :
+        self.data = index + 1
+        self.x = x
+        self.left = None
+        self.right = None
+
+class Tree :
+    def __init__(self, root,nodeDict) :
+        self.root = root
+        self.nDict = nodeDict
+
+
+            
+        
+
+
+
+
+def solution(nodeinfo):
+    answer = [[]]
+    nodeDict = dict()
+    rootKey = 0
+    for i in range(len(nodeinfo)) :
+        x = nodeinfo[i][0]
+        y = nodeinfo[i][1]
+        if rootKey < y :
+            rootKey = y
+        if y in nodeDict :
+            nodeDict[y].append([x, Node(i, x)])
+        else :
+            nodeDict[y] = deque([[x, Node(i, x)]])
+    
+    t = deque(sorted(nodeDict.items(), key= lambda x: -x[0]))
+    test = t[1]
+    alpa = deque(sorted(test[1], key= lambda x: x[0]))
+    while alpa :
+        a = alpa.popleft()
+        print(a)
+    
+    return answer
+
+
+
+print(solution([[5, 3], [11, 5], [13, 3], [3, 5], [6, 1], [1, 3], [8, 6], [7, 2], [2, 2]]))
