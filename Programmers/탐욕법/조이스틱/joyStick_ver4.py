eb25420424f52c118e8445dad10e2294f3d30ef7
misseop25 <richard25@naver.com>
2020-09-24 17:15:37 +0900

    Create joyStick_ver4.py

---
+++ Programmers/탐욕법/조이스틱/joyStick_ver4.py
@@ -0,0 +1,47 @@
+def solution(name):
+    ansList = []
+    answer = 0
+    a_len = 0
+    
+    def findA() :
+        A_Index = 0
+        a_len = 0
+        temp =[]
+        while A_Index > -1 :
+            if "A" in name[A_Index :] :
+                A_Index = name.find("A", A_Index + a_len)
+                if A_Index < 0 : break
+                a_len = 0
+                for a in name[A_Index :] :
+                    if a == "A" :
+                        a_len += 1
+                    else :
+                        break
+                temp.append([A_Index, a_len])
+            else :
+                return temp
+        return temp
+    aList = findA()
+    if aList :
+        aList = sorted(aList, key= lambda x: -x[1])
+        A_Index = aList[0][0]
+        a_len = aList[0][1]
+    else :
+        A_Index = -1
+
+
+
+    for i in name :
+        answer += min(abs(ord("A")- ord(i)), abs(ord("Z") - ord(i) + 1))
+    answer += len(name) - 1
+    ansList.append(answer)
+    if A_Index > -1 :
+        answer = 0
+        for i in name[: A_Index] :
+            answer += min(abs(ord("A")- ord(i)), abs(ord("Z") - ord(i) + 1)) + 1
+        answer += A_Index -1
+
+        for i in range(len(name)-1,A_Index + a_len -1, -1) :
+            answer += min(abs(ord("A")- ord(name[i])), abs(ord("Z") - ord(name[i]) + 1)) + 1
+        ansList.append(answer-1)
+    return min(ansList)
