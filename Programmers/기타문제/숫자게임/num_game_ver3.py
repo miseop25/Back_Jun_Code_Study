b4eab974eb43ae016f01b71226362aaf11d11954
misseop25 <richard25@naver.com>
2020-10-10 22:51:15 +0900

    Update num_game_ver3.py

--- Programmers/기타문제/숫자게임/num_game_ver3.py
+++ Programmers/기타문제/숫자게임/num_game_ver3.py
@@ -1,19 +1,15 @@
 from collections import deque
 def solution(A, B):
     answer = 0
-    result = []
-    b = deque(B)
-    a = deque(A)
+    b = deque(sorted(B, key= lambda x: -x))
+    a = deque(sorted(A, key= lambda x: -x))
     while a :
-        comp = a.popleft()
-        b = deque(sorted(b, key=lambda x: x if comp < x else (-x + 1000000000) ))
-        if b[0] > comp :
+        if a[0] < b[0] :
+            a.popleft()
+            b.popleft()
             answer += 1
-            result.append(b.popleft())
         else :
-            result.append(b.pop())
-       
-
+            a.popleft()
     return answer
 
 print(solution([5, 1, 3, 7], [2, 2, 6, 8]))
