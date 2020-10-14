e6ad7607badda29499285c5ca0b88d271c56c0b9
misseop25 <richard25@naver.com>
2020-10-14 23:19:40 +0900

    Update gemsShopping_ver1.py

--- Programmers/기타문제/보석쇼핑/gemsShopping_ver1.py
+++ Programmers/기타문제/보석쇼핑/gemsShopping_ver1.py
@@ -8,9 +8,10 @@ def solution(gems):
 
     i = 0
     j = 0
-    flag = True
+    flag = True     #구간을 줄일지 늘릴지 결정하는 Flag
 
     while j < len(gems) or not flag :
+        # 구간이 줄어든다면 j가 끝까지 갔어도 계속 진행
         if flag :
             if gems[j] not in myGems :
                 myGems[gems[j]] = 1
@@ -31,9 +32,8 @@ def solution(gems):
         else :
             flag = True
 
-
+    # 정답 후보에서 그 차이가 가장 작은 값을 중심으로 정렬
     result = sorted(answer, key= lambda x: ((x[1]-x[0]), x[0])   )
-
     return result[0]
 
 print(solution(["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]))
