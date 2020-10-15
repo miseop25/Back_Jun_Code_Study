5115f5ab778a2769e16dd3ef6a48bda655674ebd
misseop25 <richard25@naver.com>
2020-10-15 21:58:24 +0900

    Update back_joon_2557_ver1.java

--- back_joon/기타/back_joon_2557_HelloWorld/back_joon_2557_ver1.java
+++ back_joon/기타/back_joon_2557_HelloWorld/back_joon_2557_ver1.java
@@ -1,5 +1,21 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
+
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");        
+        String  a = new String("Hello World!");
+        System.out.println(a);        
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i = 0; i <10; i++) {
+            que.add(i);
+        }
+        for(int i = 0; i <5; i++) {
+            System.out.println(que.poll());
+        }
+        System.out.println(que);
     }
 }
