72969f416f3bb11e55a89f27ed4cab8c2935bf7d
misseop25 <richard25@naver.com>
2020-10-24 11:51:34 +0900

    Create back_joon_2605_ver1.py

---
+++ back_joon/구현/back_joon_2605_줄세우기/back_joon_2605_ver1.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    N = int(input())
+    num = list(map(int, input().rstrip().split(" ")  ))
+    stack_A = []
+    stack_B = []
+    data = 1
+    for i in num :
+        for _ in range(i) :
+            stack_B.append(stack_A.pop())
+        stack_A.append(str(data))
+        for _ in range(i) :
+            stack_A.append(stack_B.pop())
+        data += 1
+    print(" ".join(stack_A))
