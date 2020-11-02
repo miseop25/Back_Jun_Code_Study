58a067f06b3b8321b6d5f0425d670fd0ed78cfcb
misseop25 <richard25@naver.com>
2020-11-02 21:46:12 +0900

    Create back_joon_17952_ver1.py

---
+++ back_joon/스택/back_joon_17952_과제는끝나지않아/back_joon_17952_ver1.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+class HomeWork :
+    def __init__(self, score, time):
+        self.score = score
+        self.time = time
+
+class Soluction :
+    def __init__(self, N) :
+        self.N = N 
+        self.stack = []
+        self.target = None
+        self.answer = 0
+    
+    def getAnswer(self) :
+        for _ in range(self.N) :
+            cmd = list(map(int, input().split(" ")))
+            if cmd[0] == 1 :
+                self.stack.append(HomeWork(cmd[1], cmd[2]-1))
+                self.target = self.stack[-1]
+            else :
+                if self.target == None :
+                    continue
+                self.target.time -= 1 
+            if self.target == None :
+                continue
+            if self.target.time == 0 :
+                self.answer += self.target.score
+                self.stack.pop()
+                if self.stack :
+                    self.target = self.stack.pop()
+                else :
+                    self.target = None
+        return self.answer
+
+
+if __name__ == "__main__":
+    N = int(input())
+    s = Soluction(N)
+    print(s.getAnswer())
