ff277f998793360c7b654d046329d2aa357afd8f
misseop25 <richard25@naver.com>
2020-11-11 22:52:34 +0900

    Update back_joon_11637_ver1.py

--- back_joon/구현/back_joon_11637_인기투표/back_joon_11637_ver1.py
+++ back_joon/구현/back_joon_11637_인기투표/back_joon_11637_ver1.py
@@ -4,13 +4,20 @@ input = sys.stdin.readline
 def soluction() :
     n = int(input())
     person = dict()
+    total = 0
     for i in range(1, n+1) :
-        person[i] = int(input())
+        temp = int(input())
+        person[i] = temp
+        total += temp
+
     result = sorted(person.items(), key = lambda x: -x[1])
+
     if result[0][1] == result[1][1] :
         return "no winner"
-    else :
+    elif result[0][1] > total//2 :
         return "majority winner " + str(result[0][0])
+    else :
+        return "minority winner " + str(result[0][0])
     
 
 
@@ -18,4 +25,3 @@ if __name__ == "__main__":
     T = int(input())
     for _ in range(T) :
         print(soluction())
-        
