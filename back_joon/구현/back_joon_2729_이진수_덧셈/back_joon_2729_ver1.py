104ea8bd34f2b680dc020a70ea43d45006997528
misseop25 <richard25@naver.com>
2020-11-25 18:18:07 +0900

    Create back_joon_2729_ver1.py

---
+++ back_joon/구현/back_joon_2729_이진수_덧셈/back_joon_2729_ver1.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T) :
+        x1, x2 = map(str, input().split(" "))
+        a = int("0b" + x1, 2)
+        b = int("0b" + x2, 2)
+        c = a + b
+        result = bin(c)
+        print(result[2:])
+
+
