96344ec34b65f800ab44f594778a4de39cb4d6f9
misseop25 <richard25@naver.com>
2021-01-03 19:27:51 +0900

    Create back_joon_1010_ver1.py

---
+++ back_joon/다이나믹프로그래밍/back_joon_1010_다리놓기/back_joon_1010_ver1.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def get_aCb(a: int, b: int) :
+    left = a
+    right = b
+    for i in range(1, b) :
+        left *= a-i
+        right *= b-i
+    return int(left/right)
+
+if __name__ == "__main__":
+    N = int(input())
+    for _ in range(N) :
+        a,b = map(int, input().split(" "))
+        print(get_aCb(b,a))
+    
+
