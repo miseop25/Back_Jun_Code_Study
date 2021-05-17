42e7513deef7fbd513de549944ec2d245ea1d1be
misseop25 <richard25@naver.com>
2021-05-17 22:36:48 +0900

    Create binary_repect_ver1.py

---
+++ Programmers/기타문제/이진변환반복하기/binary_repect_ver1.py
@@ -0,0 +1,26 @@
+from collections import deque
+class Soluction :
+    def __init__(self, s):
+        self.count = 0
+        self.zeroCnt = 0
+        self.s = s
+        self.answer = 0
+    
+    def getAnswer(self) :
+        length = len(self.s)
+        while length != 1 :
+            nowCnt = self.s.count("1")
+            self.zeroCnt += length - nowCnt
+            self.s = format(nowCnt, 'b')
+            length = len(self.s)
+            self.count += 1
+
+        return [self.count, self.zeroCnt]
+
+
+
+def solution(s):
+    answer = []
+    s = Soluction(s)
+    answer = s.getAnswer()
+    return answer
