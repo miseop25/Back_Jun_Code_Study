551f68c61a715114ce519080e1271a969b357329
misseop25 <richard25@naver.com>
2021-09-04 21:23:43 +0900

    Update Vowel_dict_ver1.py

--- Programmers/기타문제/모음사전/Vowel_dict_ver1.py
+++ Programmers/기타문제/모음사전/Vowel_dict_ver1.py
@@ -1,6 +1,25 @@
 class Vowel :
     def __init__(self):
+        self.num = 1
+        self.wordDict = dict()
+        self.baseWord = ['A', 'E','I','O','U']
+    
+    def makeWord(self, target) :
+        for i, v in enumerate(self.baseWord) :
+            temp = target + v
+            if len(temp) < 6 :
+                self.wordDict[temp] = self.num
+                self.num += 1
+                self.makeWord(temp)
+            else :
+                break
+
 
 def solution(word):
     answer = 0
+    v = Vowel()
+    v.makeWord('')
+    answer = v.wordDict[word]
     return answer
+
+print(solution("EIO"))
