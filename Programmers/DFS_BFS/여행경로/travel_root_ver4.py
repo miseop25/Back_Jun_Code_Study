41d5ceaff841efdd44b201529096c8846eb9ab45
misseop25 <richard25@naver.com>
2021-09-10 23:09:38 +0900

    Create travel_root_ver4.py

---
+++ Programmers/DFS_BFS/여행경로/travel_root_ver4.py
@@ -0,0 +1,10 @@
+
+class Travel :
+    def __init__(self, ti)
+
+def solution(tickets):
+    answer = []
+    return answer
+
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
+print(solution( [['ICN','A'],['ICN','A'],['A','ICN']]))
