a24340498e25e867815c30cdb174c5751258b50f
misseop25 <richard25@naver.com>
2021-09-12 15:39:23 +0900

    Update travel_root_ver4.py

--- Programmers/DFS_BFS/여행경로/travel_root_ver4.py
+++ Programmers/DFS_BFS/여행경로/travel_root_ver4.py
@@ -1,7 +1,27 @@
 
+class AriportInfo :
+    def __init__(self, name):
+        self.myArea = name
+        self.dest = dict()
+
 class Travel :
     def __init__(self, tickets) :
         self.ticket = tickets
+        self.travelInfo = dict()
+    
+    def makeInfo(self) :
+        ticketIndex = 1
+        for st,end in self.ticket :
+            if st not in self.travelInfo :
+                self.travelInfo[st] = AriportInfo(st)
+            if end not in self.travelInfo :
+                self.travelInfo[end] = AriportInfo(end)
+                
+            self.travelInfo[st].dest[end]
+            
+
+
+
         
 
 def solution(tickets):
