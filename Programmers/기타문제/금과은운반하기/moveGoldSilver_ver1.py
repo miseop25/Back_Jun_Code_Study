7f298807bec5361ba17c10ce15294f54d498fb18
misseop25 <richard25@naver.com>
2021-09-18 21:59:00 +0900

    Create moveGoldSilver_ver1.py

---
+++ Programmers/기타문제/금과은운반하기/moveGoldSilver_ver1.py
@@ -0,0 +1,18 @@
+class MoveGlodSilver :
+    def __init__(self,a,b,g,s,w,t):
+        self.gold = a
+        self.silver = b
+
+        self.cityDict = dict()
+        self.makeCityDict(g,s,w,t)
+    
+    def makeCityDict(self, g,s,w,t) :
+        for i in range(len(g)) :
+            canMoveOne = w[i] if g[i] >= w[i] else g[i]
+            self.cityDict[i] = [g[i], s[i],w[i],t[i], canMoveOne]
+
+
+
+def solution(a, b, g, s, w, t):
+    answer = -1
+    return answer
