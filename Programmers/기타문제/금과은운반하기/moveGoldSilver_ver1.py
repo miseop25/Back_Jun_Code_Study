ed62e4639207b93242cb253fe4418b4f5d2f3a97
misseop25 <richard25@naver.com>
2021-09-30 14:33:37 +0900

    Update moveGoldSilver_ver1.py

--- Programmers/기타문제/금과은운반하기/moveGoldSilver_ver1.py
+++ Programmers/기타문제/금과은운반하기/moveGoldSilver_ver1.py
@@ -1,3 +1,6 @@
+import deque from collections
+import heapq
+
 class CityInfo :
     def __init__(self,g,s,w,t):
         self.cGold = g
@@ -11,6 +14,10 @@ class MoveGlodSilver :
     def __init__(self,a,b,g,s,w,t):
         self.gold = a
         self.silver = b
+        self.timeHeap = []
+        self.timeDict = dict()
+        self.makeTimeDict(g,s,w,t)
+
         self.cityDict = dict()
         self.makeCityDict(g,s,w,t)
     
@@ -19,6 +26,19 @@ class MoveGlodSilver :
             canMoveOne = w[i] if g[i] >= w[i] else g[i]
             # self.cityDict[i] = [g[i], s[i],w[i],t[i], canMoveOne]
             self.cityDict[i] = CityInfo(g[i], s[i],w[i],t[i])
+    
+    def makeTimeDict(self, g,s,w,t) :
+        for i in range(len(t)) :
+            newCity = CityInfo(g[i], s[i],w[i],t[i])
+            if(t[i] not in self.timeDict) :
+                self.timeDict[t[i]] = []
+                heapq.heappush(self.timeHeap, t[i])
+            self.timeDict[t[i]].append(newCity)
+
+    def makeAnswer(self) :
+        while self.timeHeap :
+            curTime = heapq.heappop(self.timeHeap)
+
 
     def getAnswer(self) :
         gTest = sorted(self.cityDict.items(), key = lambda x: ((self.gold - x[1].canMoveGold), x[1].cTime )   )
